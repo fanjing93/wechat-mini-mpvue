d5789195d3d2236b11a42ffd3857d0a07ef40135
fanjing <fanjing@wemart.cn>
2019-03-14 18:35:19 +0800

    update

---
+++ src/pages/center/index.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    },
+
+    components: {},
+
+    methods: {},
+
+    created () {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
